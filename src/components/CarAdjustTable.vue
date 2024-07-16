d6535d3c2116084af8dd8dfa68ca4ebe7d38056e
Enzo <d900124@gmail.com>
2024-07-16 17:46:58 +0800

    分頁OK / 正在寫彈出詳細視窗

--- src/components/CarAdjustTable.vue
+++ src/components/CarAdjustTable.vue
@@ -1,38 +1,44 @@
 <template>
-        <!-- https://getbootstrap.com/docs/5.2/content/tables/#table-group-dividers 表單部分 --> 
-        <!-- https://element-plus.org/zh-CN/component/config-provider.html  頁碼 -->
-    
+<!-- https://getbootstrap.com/docs/5.2/content/tables/#table-group-dividers 表單部分 --> 
+<!-- https://element-plus.org/zh-CN/component/config-provider.html  頁碼 -->
 
             <div class="col-1"></div>
+<!-- 多選下拉選單(簡易搜尋) -->
             <div class="col-5" style="padding: 0px 0px;"></div>
+
+<!-- 抬頭 -->
             <div class="col-5" style="padding: 0px 0px;">
                 <h3 class="table-title">車輛調整簽核</h3>
             </div>
             <div class="col-1"></div>
 
+
+
             <div class="col-1"></div>
             <div class="col-10">
+<!-- 彈出式複雜搜尋 -->
                 <div class="extra-menu"></div>
+<!-- 列表主體 -->
                 <div class="table-part">
                 <table class="table">
                 <thead style="border-bottom: 2px solid #a33238;">
                     <tr>
-                    <th scope="col" class="table-th" >ID</th>
-                    <th scope="col" class="table-th" >Brand - Model</th>
-                    <th scope="col" class="table-th" >Request</th>
-                    <th scope="col" class="table-th" >Time</th>
-                    <th scope="col" class="table-th" >Salse</th>
-                    <th scope="col" class="table-th" >State</th>
+                    <th scope="col" class="table-th" >簽核編號</th>
+                    <th scope="col" class="table-th" >品牌 - 型號</th>
+                    <th scope="col" class="table-th" >簽核需求</th>
+                    <th scope="col" class="table-th" >建立時間</th>
+                    <th scope="col" class="table-th" >建立者</th>
+                    <th scope="col" class="table-th" >簽核狀態</th>
                     </tr>
                 </thead>
                 <tbody class="table-group-divider">
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
+                    <tr v-for="item in items" :key="item.id"  @click="itemClick(item.id)">
+                    <th scope="row" class="table-td">{{item.id}}</th>
+                    <td class="table-td"></td>
+                    <td class="table-td">{{item.approvalTypeName}} {{item.floatingAmount}}</td>
+                    <td class="table-td">{{item.createTimeString}}</td>
+                    <td class="table-td">{{item.employeeName}}</td>
+                    <td class="table-td">{{item.approvalStatusName}}</td>
                     </tr>
                     
                 </tbody>
@@ -45,19 +51,38 @@
         <div class="col-1"></div>
 
         <div class="col-1"></div>
-        <div class="col-10" style="background-color: unset; justify-content:right">
-            
+        <div class="col-5" style="padding: 0px 0px;background-color: unset;  display: flex; justify-content: flex-start;">
+<!-- 新增用按鈕 -->
+            <div class="btm-div" style="display: flex;" @click="openModal('insert')">
+            <font-awesome-icon icon="plus" size="xl" style="color: #a33238; padding: 13 5 0 5;"/>
+            <el-button type='' link class="text-btm" style="color: #a33238;">新增簽核</el-button>
+            </div>
+        </div>
+
+        <div class="col-5" style="padding: 0px 0px;background-color: unset;  display: flex; justify-content: flex-end; ">
+<!-- 分頁區塊 -->
             <el-pagination
-                hide-on-single-page="true"
+                style="margin: 10px 0px;"
+                hide-on-single-page=true
                 layout="total,prev, pager, next"
                 :total="total"
-                style="margin: 10px 0px;"
-                :page-size="pageSize"
+                :page-size="rows"
+                v-model:current-page="current"
+                @change="callFindByHQL"
             ></el-pagination>
             
 
             </div>
         <div class="col-1"></div>
+
+
+<!-- 下方詳細資料區 -->
+
+        <div v-if="openZon">
+            <div class="col-1"></div>
+            <div class="col-10" style="height: 300px;"></div>
+            <div class="col-1"></div>
+        </div>
 </template>
     
 <script setup >
@@ -73,8 +98,10 @@ const router = useRouter()
 const total = ref(0) //總比數
 const current = ref(1) //目前頁碼
 const pages = ref(0) //分頁總數
-const starts = ref(0) //起始資料位址
-const pageSize = ref(7) //每頁數量
+const rows = ref(4) //分頁資料顯示筆數
+
+//下方詳細資料開啟用
+const openZon = ref(true)
 
 //產品顯示products元件用的參數
 const items = ref([]);
@@ -83,19 +110,32 @@ onMounted(function () {
     callFindByHQL();
 })
 
-function callFindByHQL(page){
-    console.log("callFindByHQL",page);
+//單筆新增
+function openModal(){
+console.log("openModal");
+}
 
-    //獲取分頁資訊
-    if (page) {
-        starts.value=(page-1)*rows.value;
-        current.value=page;
-    }else{
-        starts.value=0;
-        current.value=1;
-    }
+//單筆查詢
+function itemClick(itemId){
+    console.log(itemId)
+    axiosapi.get("/carAdjust/"+itemId).then(function (responce) {  //(AJAX前端程式)單筆查詢的Post功能()
+        console.log("responce",responce.data);
+    }).catch(function (error) {
+        console.log("error",error);
+        Swal.fire({
+                text: "查詢錯誤"+error.message,
+                icon: "error"
+            });
+        // router.push("/")
+    }) 
+}
 
-    let request ={ //查詢的request將 "分頁-起始資料位址" 和 "分頁-顯示筆數" 填入
+
+//多筆查詢
+function callFindByHQL(){
+    console.log("callFindByHQL - 當前頁碼:",current.value);
+
+    let request ={ 
         "id":null, 
         "viewCarDateStr":null, 
         "viewCarDateEnd":null, 
@@ -107,19 +147,18 @@ function callFindByHQL(page){
         "suspensionid":null, 
         "assignedStatus":null,
 
-        "isPage":0,
-        "max":3,
+        "isPage":current.value-1,
+        "max":rows.value,
         "dir":true,
         "order":"id"
     }
 
-    axiosapi.post("/viewCarAssigned/findByHQL",request).then(function (responce) {  //(AJAX前端程式)多產品查詢的Post功能()
+    axiosapi.post("/carAdjust/findByHQL",request).then(function (responce) {  //(AJAX前端程式)多產品查詢的Post功能()
         items.value = responce.data.data;
 
         //計算分頁比數資訊
         total.value = responce.data.totalElement;
         pages.value = responce.data.totalPage;
-        pageSize.value = responce.data.numberOfElementsOnPage
 
         console.log("items",responce.data.data);
         console.log("total",responce.data.totalElement);
@@ -141,6 +180,19 @@ function callFindByHQL(page){
 </script>
     
 <style scoped>
+.btm-div:hover{
+    text-decoration:underline 2px solid #a33238;
+}
+
+.text-btm{
+    font-size: 1.2em ;
+    font-weight: 900;
+}
+
+.table-td{
+    font-size: 0.8em ;
+}
+
 .table-th{
     color: #a33238;
 }
