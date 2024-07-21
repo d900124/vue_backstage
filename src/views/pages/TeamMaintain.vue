bbb26b2d113ba389063567fe5816960dcbed39c2
sasha <yellow19950217@gmail.com>
2024-07-22 02:03:24 +0800

    登入

--- src/views/pages/TeamMaintain.vue
+++ src/views/pages/TeamMaintain.vue
@@ -1,6 +1,6 @@
 <template>
-    
-    <div class="container text-center" >
+
+    <div class="container text-center">
         <div class="row">
             <!-- 車輛調整簽核 -->
             <CarAdjustTable></CarAdjustTable>
@@ -10,9 +10,10 @@
 
             <!-- 員工總覽 -->
             <Employee></Employee>
-            
+
 
             <!-- 考核評比 -->
+            <LoginForm></LoginForm>
 
         </div>
     </div>
@@ -22,11 +23,10 @@
 import CarAdjustTable from '@/components/CarAdjustTable.vue';
 import Leave from '@/components/Leave.vue';
 import EmployeeList from '@/components/Employee.vue';
+import LoginForm from '@/components/LoginForm.vue';
+
 
 
 </script>
-    
-<style>
 
-    
-</style>
+<style></style>
