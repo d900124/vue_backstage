ffd557f5cda1d1445e9a825e4046aa9f538685b4
sasha <yellow19950217@gmail.com>
2024-07-23 13:19:02 +0800

    優化登入

--- src/views/pages/LoginPage.vue
+++ src/views/pages/LoginPage.vue
@@ -1,140 +1,201 @@
 <template>
-	<form @submit.prevent="handleSubmit">
-		<div class="mb-3">
-			<label for="exampleInputEmail1" class="form-label">帳號</label>
-			<input type="text" class="form-control" id="exampleInputEmail1" v-model="username"
-				aria-describedby="emailHelp" required>
+	<el-card class="card">
+		<div class="header-container">
+			<RouterLink :to="{ name: 'home-link' }" class="logo">
+				<el-icon class="logo-icon"><img src="/Kajarta_LOGO_02.svg"></el-icon>
+			</RouterLink>
+			<span class="header-text">員工登入</span>
 		</div>
-		<div class="mb-3">
-			<label for="exampleInputPassword1" class="form-label">密碼</label>
-			<input type="password" class="form-control" id="exampleInputPassword1" v-model="password" required>
+		<div class="card-content">
+		
+			<img src="/jeep.jpg" class="card-image">
+			<div class="form-container">
+				<form @submit.prevent="handleSubmit" class="form">
+					<br>
+					<br>
+					<br>
+					<p class="p-text">Life's tough,<br>but working for a living must go on.</p><br>
+					
+					<div class="form-group">
+						<label for="username">帳號:</label>
+						<input type="text" id="username" v-model="username" class="input-field" required>
+					</div>
+					<div class="form-group">
+						<label for="password">密碼:</label>
+						<input type="password" id="password" v-model="password" class="input-field" required>
+					</div>
+					<button type="submit" class="submit-button">登入</button>
+					<div v-if="loginMessage" class="alert alert-success mt-3">
+						{{ loginMessage }}
+					</div>
+				</form>
+			</div>
 		</div>
-		<button type="submit" class="btn btn-primary">登入</button>
-		<div v-if="loginMessage" class="alert alert-success mt-3">
-			{{ loginMessage }}
-		</div>
-	</form>
+	</el-card>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { useRouter } from 'vue-router'; 
+import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
-import Swal from 'sweetalert2'; 
+import Swal from 'sweetalert2';
 import axiosapi from '@/plugins/axios';
 
 const username = ref('');
 const password = ref('');
-const loginMessage = ref(''); 
-const router = useRouter(); 
+const loginMessage = ref('');
+const router = useRouter();
 const store = useStore();
 
 const handleSubmit = () => {
-	// let request = {
-	// 	username: username.value,
-	// 	password: password.value
-	// };
-
-	// console.log('Request:', request); // 打印 request 確認數據
-
-	// 创建 FormData 对象
-    const formData = new FormData();
-    formData.append('username', username.value);
-    formData.append('password', password.value);
-
-    console.log('FormData:', formData); // 打印 FormData 确认数据
+	const formData = new FormData();
+	formData.append('username', username.value);
+	formData.append('password', password.value);
 
 	axiosapi.post(`/login`, formData, {
 		headers: {
 			'Content-Type': 'multipart/form-data'
 		},
-		withCredentials: true // 允许跨域請求带上 Cookie
+		withCredentials: true
 	})
 		.then(response => {
 			const data = response.data;
-			console.log('Response data:', data); 
 
 			if (data.success && data.code === 200) {
-				// 登入成功處理
-				console.log('Login successful', data);
 				Swal.fire({
 					icon: 'success',
 					title: '登入成功',
 					text: '您已成功登入！'
-				}).then(async() => {
+				}).then(async () => {
 					const username = data.data.username;
-					console.log('=====>username', username);
 
 					localStorage.setItem('username', username);
-					// 发起请求获取员工信息
 					await getEmployeeInfo(username);
-					// 跳轉到 Home 頁面
 					router.push({ name: 'personal-link' });
 				});
 			} else {
-				// 處理非200狀態碼或 success 為 false 的情况
-				console.error('Login failed with response:', response);
 				Swal.fire({
 					icon: 'error',
-					title: '登入失败',
+					title: '登入失敗',
 					text: data.msg || '帳號或密碼錯誤'
 				});
 			}
 		})
 		.catch(error => {
-			// 處理其他错误
-			console.error('Login failed', error);
 			Swal.fire({
 				icon: 'error',
-				title: '登入失败',
+				title: '登入失敗',
 				text: '帳號或密碼錯誤'
 			});
 		});
 };
 
-// 使用 async/await 文法主要有以下幾個好處：
-// 1.程式碼可讀性：async/await 語法使非同步程式碼看起來更像同步程式碼，從而提高程式碼的可讀性和可維護性。相較於 .then() 鍊式調用，async/await 更加直觀，減少了回調地獄的問題。
-// 2.錯誤處理：async/await 語法可以使用標準的 try/catch 區塊來處理錯誤，使得錯誤處理更加簡潔一致。在 .then() 鍊式呼叫中，你通常需要在鏈的末端使用 .catch() 來處理錯誤。
-// 3.調試方便：調試使用 async/await 的程式碼比 .then() 鍊式呼叫更方便，因為堆疊追蹤資訊更加清晰。你可以更輕鬆地查看程式碼執行的順序和變數的狀態。
-// 获取员工信息
 const getEmployeeInfo = async (username) => {
-    try {
-        const response = await axiosapi.get(`/employee/info/username/${username}`);
-        const data = response.data;
-        console.log('Employee info:', data);
-		// 假设我们使用 Vuex 来存储员工信息
-        store.commit('setEmployeeInfo', data);
-    } catch (error) {
-        console.error('Failed to get employee info:', error);
-    }
+	try {
+		const response = await axiosapi.get(`/employee/info/username/${username}`);
+		const data = response.data;
+		store.commit('setEmployeeInfo', data);
+	} catch (error) {
+		console.error('Failed to get employee info:', error);
+	}
 };
-// 使用.then
-// const getEmployeeInfo = (username) => {
-//     axiosapi.get(`/employee/info/${username}`)
-//     .then(response => {
-//         const data = response.data;
-//         console.log('Employee info:', data);
-//     })
-//     .catch(error => {
-//         console.error('Failed to get employee info:', error);
-//     });
-// };
 </script>
 
 <style scoped>
-form {
+.p-text{
+
+
+	
+	
+	width: 90%; /* 调整底线的长度 */
+	border-bottom: 1px solid #a33238;
+}
+.header-container {
 	display: flex;
-	flex-direction: column;
-	max-width: 300px;
-	margin: auto;
+	align-items: center;
+	margin-bottom: 20px;
 }
 
-div {
+.logo {
+	all: unset;
+	margin-right: 30px;
+	display: flex;
+	justify-content: center;
+}
+
+.logo-icon {
+	width: 30px;
+}
+
+.header-text {
+	font-size: 1.5em;
+	font-weight: bold;
+	color: #a33238;
+}
+
+.card {
+	max-width: 800px;
+	margin: auto;
+	padding: 20px;
+	background-color: #fff5eb;
+}
+
+.card-content {
+	display: flex;
+	align-items: flex-start;
+	background-color: white;
+	font-weight: bold;
+	color: #a33238;
+}
+
+.card-image {
+	width: 50%;
+	margin-right: 20px;
+}
+
+.form-container {
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+
+.form {
+	width: 100%;
+	max-width: 400px;
+}
+
+.form-group {
 	margin-bottom: 15px;
 }
 
-button {
+.form-group label {
+	margin-right: 10px;
+}
+
+.input-field {
+	width: calc(100% - 100px);
+	/* 计算宽度以适应标签 */
+	padding: 8px;
+	border: none;
+	border-bottom: 1px solid #a33238;
+	/* 底線 */
+	background-color: transparent;
+}
+
+.submit-button {
+	width: 100%;
+	max-width: 300px;
 	padding: 10px;
+	border: none;
+	border-radius: 4px;
+	background-color: #a33238;
+	color: white;
+	cursor: pointer;
+}
+
+.submit-button:hover {
+	background-color: #66b1ff;
 }
 
 .alert {
